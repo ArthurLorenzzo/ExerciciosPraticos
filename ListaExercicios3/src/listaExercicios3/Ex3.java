cefc8393093c125dce020e785e9963fc995da20a
GustavoSilvalgs <luisgust06@gmail.com>
2023-08-06 17:39:02 -0300

    Exercise 3 sent

--- ListaExercicios3/src/listaExercicios3/Ex3.java
+++ ListaExercicios3/src/listaExercicios3/Ex3.java
@@ -1,2 +1,27 @@
-package listaExercicios3;public class Ex3 {
+package listaExercicios3;
+
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of men in the class: ");
+        int men = sc.nextInt();
+
+        System.out.print("Enter the number of woman in the class: ");
+        int women = sc.nextInt();
+
+        int total = men + women;
+
+        double percentMen = ((double) men / total) * 100;
+        double percentWomen = ((double) women / total) * 100;
+
+        System.out.println("Total students in the class:");
+        System.out.printf("Percentage of men: %.2f%%\n", percentMen);
+        System.out.printf("Percentage of women: %.2f%%\n", percentWomen);
+
+        sc.close();
+    }
 }
