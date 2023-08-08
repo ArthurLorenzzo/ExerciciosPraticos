5d85ccb2aca48b4e44a3daea417cbcaae954339a
GustavoSilvalgs <luisgust06@gmail.com>
2023-08-08 18:06:58 -0300

    Including exercise 04 Heranca

---
+++ ListaExercicios3/src/heranca/Administrador.java
@@ -0,0 +1,24 @@
+package heranca;
+
+public class Administrador extends Empregado {
+
+    private double ajudaDeCusto;
+
+    public Administrador(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double ajudaDeCusto) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    public double getAjudaDeCusto() {
+        return ajudaDeCusto;
+    }
+
+    public void setAjudaDeCusto(double ajudaDeCusto) {
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    public double calcularSalario() {
+        double salarioCalculado = super.calcularSalario();
+        return salarioCalculado + ajudaDeCusto;
+    }
+}
--- ListaExercicios3/src/heranca/Main.java
+++ ListaExercicios3/src/heranca/Main.java
@@ -8,6 +8,19 @@ public class Main {
 		
 		p1.obterSaldo();
 
+//		Testando Classe Administrador
+		Administrador adm = new Administrador("Gustavo", "São Paulo", "(11)99999-9999",
+				5, 5000.0, 10.0, 1000.0);
+
+		System.out.println("Nome: " + adm.getNome());
+		System.out.println("Endereço: " + adm.getEndereco());
+		System.out.println("Telefone: " + adm.getTelefone());
+		System.out.println("Código setor: " + adm.getCodigoSetor());
+		System.out.println("Salário base: " + adm.getSalarioBase());
+		System.out.println("Imposto: " + adm.getImposto());
+		System.out.println("Ajuda de custo: " + adm.getAjudaDeCusto());
+		System.out.println("Salário total: " + adm.calcularSalario());
+
 	}
 
 }
